bba6ec61c4e881d6bfa0b7b4f241555567a125e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-07 23:38:36 -0500

    3 tests for 'something else' working

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,36 +2,59 @@ describe('array-explorer', () => {
   beforeEach(() => {
     cy.visit('http://localhost:8080')
   })
+
+  // utility functions
+  const selectMethodOptions = choice => cy.get('#methodoptions').select(choice)
+  const confirmInputAndOutput = () => {
+    let output
+    cy
+      .get('.exampleoutput2')
+      .invoke('text')
+      .then(t => {
+        output = t
+      })
+    // set up spy on `console.log` before
+    // we can call `eval(input code)`
+    cy.spy(console, 'log')
+    cy.get('.usage1').then(v => {
+      const input = v.text()
+      // evaluate the input code - we are already spying on console.log!
+      eval(input)
+      // the value comes from DOM - so it needs to be
+      // converted before we can compare it to the
+      // compute value
+      expect(console.log).to.have.been.calledWith(JSON.parse(output))
+      cy
+        .get('.exampleoutput2')
+        .should('have.css', 'opacity', '1')
+        .and('contain', String(output))
+    })
+  }
+
   it('loads', () => {
     cy.contains('h1', 'JavaScript Array Explorer')
   })
+
   context('something else', () => {
-    it('shows length of an array', () => {
+    beforeEach(() => {
       cy.get('#firstmethod').select('something else')
-      cy.get('#methodoptions').select('find the length of the array')
-      let output
-      cy
-        .get('.exampleoutput2')
-        .invoke('text')
-        .then(t => {
-          output = t
-        })
-      // set up spy on `console.log` before
-      // we can call `eval(input code)`
-      cy.spy(console, 'log')
-      cy.get('.usage1').then(v => {
-        const input = v.text()
-        // evaluate the input code - we are already spying on console.log!
-        eval(input)
-        // the value comes from DOM - so it needs to be
-        // converted before we can compare it to the
-        // compute value
-        expect(console.log).to.have.been.calledWith(JSON.parse(output))
-        cy
-          .get('.exampleoutput2')
-          .should('have.css', 'opacity', '1')
-          .and('have.text', String(output))
-      })
+    })
+
+    it('shows length of an array', () => {
+      selectMethodOptions('find the length of the array')
+      confirmInputAndOutput()
+    })
+
+    it('fills array with given value', () => {
+      selectMethodOptions(
+        'fill all the elements of the array with a static value'
+      )
+      confirmInputAndOutput()
+    })
+
+    it('copy a sequence of array elements within the array', () => {
+      selectMethodOptions('copy a sequence of array elements within the array.')
+      confirmInputAndOutput()
     })
   })
 })
