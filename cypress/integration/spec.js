fabacc94afcc89c0c19b9b7ad16ebe6fbc9d2bae
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 00:04:38 -0500

    generating tests from object of method names

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -5,6 +5,7 @@ describe('array-explorer', () => {
 
   // utility functions
   const selectMethodOptions = choice => cy.get('#methodoptions').select(choice)
+  const toDouble = text => text.replace(/'/g, '"')
   const confirmInputAndOutput = () => {
     let output
     cy
@@ -23,11 +24,12 @@ describe('array-explorer', () => {
       // the value comes from DOM - so it needs to be
       // converted before we can compare it to the
       // compute value
-      expect(console.log).to.have.been.calledWith(JSON.parse(output))
+      // TODO remove comments from the output
+      expect(console.log).to.have.been.calledWith(JSON.parse(toDouble(output)))
       cy
         .get('.exampleoutput2')
         .should('have.css', 'opacity', '1')
-        .and('contain', String(output))
+        .and('contain', output)
     })
   }
 
@@ -35,26 +37,52 @@ describe('array-explorer', () => {
     cy.contains('h1', 'JavaScript Array Explorer')
   })
 
-  context('something else', () => {
-    beforeEach(() => {
-      cy.get('#firstmethod').select('something else')
-    })
+  const methods = {
+    'add items or other arrays': [
+      'element/s to an array',
+      'elements to the end of an array',
+      'elements to the front of an array',
+      'this array to other array(s) and/or value(s)',
+    ],
+    'remove items': [
+      'element/s from an array',
+      'the last element of the array',
+      'the first element of the array',
+      'one or more elements in order for use, leaving the array as is',
+    ],
+    // skip "find items" - requires multiple parameters
+    'walk over items': [
+      'executing a function I will create for each element',
+      'creating a new array from each element with a function I create',
+      'creating an iterator object',
+    ],
+    'return a string': [
+      'join all elements of the array into a string',
+      'return a string representing the array.',
+      'return a localized string representing the array.',
+    ],
+    'order an array': [
+      'reverse the order of the array',
+      'sort the items of the array',
+    ],
+    'something else': [
+      'find the length of the array',
+      'fill all the elements of the array with a static value',
+      'copy a sequence of array elements within the array.',
+    ],
+  }
 
-    it('shows length of an array', () => {
-      selectMethodOptions('find the length of the array')
-      confirmInputAndOutput()
-    })
-
-    it('fills array with given value', () => {
-      selectMethodOptions(
-        'fill all the elements of the array with a static value'
-      )
-      confirmInputAndOutput()
-    })
-
-    it('copy a sequence of array elements within the array', () => {
-      selectMethodOptions('copy a sequence of array elements within the array.')
-      confirmInputAndOutput()
+  Object.keys(methods).forEach(method => {
+    context(method, () => {
+      beforeEach(() => {
+        cy.get('#firstmethod').select(method)
+      })
+      methods[method].forEach(secondary => {
+        it(secondary, () => {
+          selectMethodOptions(secondary)
+          confirmInputAndOutput()
+        })
+      })
     })
   })
 })
