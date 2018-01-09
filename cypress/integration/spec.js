2176810e1c6bbeae97921b8f3b7f7b4f7f8d34c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-01-08 23:20:31 -0500

    enable all tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -95,42 +95,39 @@ describe('array-explorer', () => {
     cy.contains('h1', 'JavaScript Array Explorer')
   })
 
-  // const methods = {
-  //   'add items or other arrays': [
-  //     'element/s to an array',
-  //     'elements to the end of an array',
-  //     'elements to the front of an array',
-  //     'this array to other array(s) and/or value(s)',
-  //   ],
-  //   'remove items': [
-  //     'element/s from an array',
-  //     'the last element of the array',
-  //     'the first element of the array',
-  //     'one or more elements in order for use, leaving the array as is',
-  //   ],
-  //   // skip "find items" - requires multiple parameters
-  //   'walk over items': [
-  //     'executing a function I will create for each element',
-  //     'creating a new array from each element with a function I create',
-  //     'creating an iterator object',
-  //   ],
-  //   'return a string': [
-  //     'join all elements of the array into a string',
-  //     'return a string representing the array.',
-  //     'return a localized string representing the array.',
-  //   ],
-  //   'order an array': [
-  //     'reverse the order of the array',
-  //     'sort the items of the array',
-  //   ],
-  //   'something else': [
-  //     'find the length of the array',
-  //     'fill all the elements of the array with a static value',
-  //     'copy a sequence of array elements within the array.',
-  //   ],
-  // }
   const methods = {
-    'return a string': ['return a localized string representing the array.'],
+    'add items or other arrays': [
+      'element/s to an array',
+      'elements to the end of an array',
+      'elements to the front of an array',
+      'this array to other array(s) and/or value(s)',
+    ],
+    'remove items': [
+      'element/s from an array',
+      'the last element of the array',
+      'the first element of the array',
+      'one or more elements in order for use, leaving the array as is',
+    ],
+    // skip "find items" - requires multiple parameters
+    'walk over items': [
+      'executing a function I will create for each element',
+      'creating a new array from each element with a function I create',
+      'creating an iterator object',
+    ],
+    'return a string': [
+      'join all elements of the array into a string',
+      'return a string representing the array.',
+      'return a localized string representing the array.',
+    ],
+    'order an array': [
+      'reverse the order of the array',
+      'sort the items of the array',
+    ],
+    'something else': [
+      'find the length of the array',
+      'fill all the elements of the array with a static value',
+      'copy a sequence of array elements within the array.',
+    ],
   }
 
   Object.keys(methods).forEach(method => {
