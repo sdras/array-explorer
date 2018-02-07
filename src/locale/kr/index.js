39e554fa5acede12ce6bdbe0a2cf1d3e2a6b363d
devjang <devjang2016@gmail.com>
2018-02-07 18:14:52 +0900

    Add Korea translation

---
+++ src/locale/kr/index.js
@@ -0,0 +1,24 @@
+export default {
+  firstMethod: '나는 배열을 갖고 있다',
+  findMethod: "찾으려는 것은",
+  methodOptions: '나에게 필요한',
+  methodTypes: {
+    add: '추가 방법은',
+    remove: '제거 방법은',
+    find: '탐색은',
+    'iterate by': '반복 방법은',
+    'string': 'string'
+  },
+  singleItem: '한가지 요소',
+  manyItems: '한가지 또는 다수의 요소',
+  primaryOptions: [
+    '한가지 요소 또는 다른 배열',
+    '요소 제거하기',
+    '요소 찾기',
+    '요소 좌지우지하기',
+    '문자열로 반환하기',
+    '배열 정렬하기',
+    '또 다른 것'
+  ]
+  // other text can be added here
+}
