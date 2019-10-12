09f328b45e7c1dd17f5f2a5b44700aa2d6d05d9a
Furkan Ozbek <furkanozbek1995@gmail.com>
2019-10-12 10:51:00 +0300

    typo fix
    I fixed some sentences in Turkish language

--- store/tr/index.js
+++ store/tr/index.js
@@ -1,38 +1,38 @@
 export default {
   state: {
-    selectedMethod: '',
+    selectedMethod: "",
     adding: [
       {
-        name: 'splice',
-        shortDesc: 'dizideki elemanlar',
-        desc: 'Dizideki elemanlardan ekle ya da sil.',
+        name: "splice",
+        shortDesc: "dizideki elemanlar",
+        desc: "Dizideki elemanlardan ekle ya da sil.",
         example: `arr.splice(2, 0, 'tacos');<br>
         console.log(arr);`,
         output: `[5, 1, 'tacos', 8]`
       },
       {
-        name: 'push',
-        shortDesc: 'dizinin sonundaki elemanlar',
+        name: "push",
+        shortDesc: "dizinin sonundaki elemanlar",
         desc:
-          'Bir dizinin sonuna bir veya daha fazla öğe ekler ve dizinin yeni uzunluğunu döndürür',
+          "Bir dizinin sonuna bir veya daha fazla öğe ekler ve dizinin yeni uzunluğunu döndürür",
         example: `arr.push(2);<br>
         console.log(arr);`,
-        output: '[5, 1, 8, 2]'
+        output: "[5, 1, 8, 2]"
       },
       {
-        name: 'unshift',
-        shortDesc: 'bir dizinin önündeki öğeler',
+        name: "unshift",
+        shortDesc: "bir dizinin önündeki öğeler",
         desc:
-          'Bir dizinin önüne bir veya daha fazla öğe ekler ve dizinin yeni uzunluğunu döndürür',
+          "Bir dizinin önüne bir veya daha fazla öğe ekler ve dizinin yeni uzunluğunu döndürür",
         example: `arr.unshift(2, 7);<br>
         console.log(arr);`,
-        output: '[2, 7, 5, 1, 8]'
+        output: "[2, 7, 5, 1, 8]"
       },
       {
-        name: 'concat',
-        shortDesc: 'dizi başka bir dizi(ler) / değer(ler)',
+        name: "concat",
+        shortDesc: "dizi başka bir dizi(ler) / değer(ler)",
         desc:
-          'Diğer dizi (ler) ve / veya değer (ler) ile birleştirilen bu diziden oluşan yeni bir dizi döndürür.',
+          "Diğer dizi (ler) ve / veya değer (ler) ile birleştirilen bu diziden oluşan yeni bir dizi döndürür.",
         example: `let arr2 = ['a', 'b', 'c'];<br>
         let arr3 = arr.concat(arr2);<br>
         console.log(arr3);`,
@@ -41,37 +41,35 @@ export default {
     ],
     removing: [
       {
-        name: 'splice',
-        shortDesc: 'dizideki elemanlar',
-        desc: 'Dizideki elemanlardan ekle ya da sil.',
+        name: "splice",
+        shortDesc: "dizideki elemanlar",
+        desc: "Dizideki elemanlardan ekle ya da sil.",
         example: `arr.splice(2, 1);<br>
         console.log(arr);`,
         output: `[5, 1]`
       },
       {
-        name: 'pop',
-        shortDesc: 'dizinin son elemanı',
-        desc:
-          'Dizinin son elemanını siler ve geri kalanı geri döndürür.',
+        name: "pop",
+        shortDesc: "dizinin son elemanı",
+        desc: "Dizinin son elemanını siler ve silinen elemanı geri döndürür.",
         example: `arr.pop();<br>
         console.log(arr);`,
         output: `[5, 1]`
       },
       {
-        name: 'shift',
-        shortDesc: 'dizinin ilk elemanı',
-        desc:
-          'Dizinin ilk elemanını siler ve geri kalanı geri döndürür',
+        name: "shift",
+        shortDesc: "dizinin ilk elemanı",
+        desc: "Dizinin ilk elemanını siler ve silinen elemanı geri döndürür",
         example: `arr.shift();<br>
         console.log(arr);`,
         output: `[1, 8]`
       },
       {
-        name: 'slice',
+        name: "slice",
         shortDesc:
-          'diziyi olduğu bırakmak için bir ya da daha fazla elemanın kullanımı',
+          "diziyi olduğu bırakmak için bir ya da daha fazla elemanın kullanımı",
         desc:
-          '<code> slice () </ code> yöntemi, bir dizinin bir bölümünün bir kopyasını yeni bir dizi nesnesine döndürür .Yalnızca başlangıç öğesini (dizinin sonuna kadar sonlanacağı nokta) veya virgülle ayrılmış hem başlangıç hem de son ögesini belirtebilirsiniz. Bununda yeni bir dizi elde edersiniz, bu orijinal dizi değişmeyecektir.',
+          "<code> slice () </ code> yöntemi, bir dizinin bir bölümünün bir kopyasını yeni bir dizi nesnesine döndürür .Yalnızca başlangıç öğesini (dizinin sonuna kadar sonlanacağı nokta) veya virgülle ayrılmış hem başlangıç hem de son ögesini belirtebilirsiniz. Bununla yeni bir dizi elde edersiniz, bu orijinal diziyi değişmeyecektir.",
         example: `let slicedArr = arr.slice(1);<br>
         console.log(arr);<br>
         console.log(slicedArr);`,
@@ -81,25 +79,25 @@ export default {
     ],
     string: [
       {
-        name: 'join',
-        shortDesc: 'dizinin tüm elemanlarını bir string haline getir',
+        name: "join",
+        shortDesc: "dizinin tüm elemanlarını bir string haline getir",
         desc: `Dizinin tüm elemanlarını bir string haline getirir.
          <code>elements.join(' - ')</code> kodu çıktı olarak <code>foo-bar</code> kodunu verir.`,
         example: `console.log(arr.join());`,
         output: `"5,1,8"`
       },
       {
-        name: 'toString',
-        shortDesc: 'bir diziyi temsil eden bir string döndür',
-        desc: 'Geriye diziyi ve öğelerini temsil eden bir string döndürür.',
+        name: "toString",
+        shortDesc: "bir diziyi temsil eden bir string döndür",
+        desc: "Geriye diziyi ve öğelerini temsil eden bir string döndürür.",
         example: `console.log(arr.toString());`,
         output: `"5,1,8"`
       },
       {
-        name: 'toLocaleString',
-        shortDesc: 'bir diziyi temsil eden yerelleştirilmiş bir string döndür',
+        name: "toLocaleString",
+        shortDesc: "bir diziyi temsil eden yerelleştirilmiş bir string döndür",
         desc:
-        'Bu biraz delice. Diziyi temsil eden yerelleştirilmiş bir string döndürür. Bu string, tarihler, para birimleri gibi durumlar için çok kullanışlıdır.',
+          "Bu biraz delice. Diziyi temsil eden yerelleştirilmiş bir string döndürür. Bu string, tarihler, para birimleri gibi durumlar için çok kullanışlıdır.",
         example: `let date = [new Date()];<br>
         const arrString = arr.toLocaleString();<br>
         const dateString = date.toLocaleString();<br>
@@ -109,18 +107,18 @@ export default {
     ],
     ordering: [
       {
-        name: 'reverse',
-        shortDesc: 'dizinin sıralamasını tersine çevir',
+        name: "reverse",
+        shortDesc: "dizinin sıralamasını tersine çevir",
         desc:
-          'Bir dizinin elemanlarının sırasını tersine çevirir - ilki sonuncu olur ve sonuncu ilk olur.',
+          "Bir dizinin elemanlarının sırasını tersine çevirir - ilki sonuncu olur ve sonuncu ilk olur.",
         example: `arr.reverse();<br>
         console.log(arr);`,
         output: `[8, 1, 5]`
       },
       {
-        name: 'sort',
-        shortDesc: 'dizinin elemanlarını sırala',
-        desc: `Bir dizinin elemanlarını yerinde sıralar ve geriye diziyi döndürür.<br>
+        name: "sort",
+        shortDesc: "dizinin elemanlarını sırala",
+        desc: `Bir dizinin elemanlarını sıralar ve geriye sıralanan diziyi döndürür.<br>
         <br>
         <strong>Önemli Not:</strong> Eğer compareFunction desteklenmiyorsa, elemanlar stringe dönüştürülüp Unicode kod sırasına göre sıralanır. Örneğin, 'Banana' elemanı 'cherry' elemanından önce gelir.  Sayısal bir sıralamada 9, 80'den önce gelir ancak Unicode düzeninde 80, 9'dan önce gelir. Dökümanlarda daha fazla bilgiye ulaşabilirsiniz.`,
         example: `arr.sort();<br>
@@ -130,26 +128,26 @@ export default {
     ],
     other: [
       {
-        name: 'length',
-        shortDesc: 'dizinin uzunluğunu bul',
-        desc: 'Geriye dizinin elemanlarının sayısını döndürür.',
+        name: "length",
+        shortDesc: "dizinin uzunluğunu bul",
+        desc: "Geriye dizinin eleman sayısını döndürür.",
         example: `console.log(arr.length);`,
         output: `3`
       },
       {
-        name: 'fill',
-        shortDesc: 'dizinin tüm öğelerini statik bir değerle doldur',
+        name: "fill",
+        shortDesc: "dizinin tüm öğelerini statik bir değerle doldur",
         desc:
-          'Bir dizinin tüm öğelerini bir başlangıç noktasından bir bitiş noktasına kadar statik değer ile doldurur.',
+          "Bir dizinin tüm öğelerini bir başlangıç noktasından bir bitiş noktasına kadar statik değer ile doldurur.",
         example: `arr.fill(2);<br>
         console.log(arr);`,
         output: `[2, 2, 2]`
       },
       {
-        name: 'copyWithin',
-        shortDesc: 'dizi içindeki dizi elemanlarının bir dizisini kopyala',
+        name: "copyWithin",
+        shortDesc: "dizi içindeki dizi elemanlarının bir dizisini kopyala",
         desc:
-          'Dizideki dizi öğelerinin bir dizisini kopyalar. Yalnızca bitiş öğesinin (başlangıçta sıfıra varsayılan olarak sıfır) veya hem başlangıç hem de son, virgülle ayrılmış olarak belirtebilirsiniz.',
+          "Dizideki dizi öğelerinin bir dizisini kopyalar. Yalnızca bitiş öğesinin (başlangıçta sıfıra varsayılan olarak sıfır) veya hem başlangıç hem de son, virgülle ayrılmış olarak belirtebilirsiniz.",
         example: `arr.copyWithin(1);<br>
         console.log(arr);`,
         output: `[5, 5, 1]`
@@ -157,10 +155,10 @@ export default {
     ],
     iterate: [
       {
-        name: 'forEach',
-        shortDesc: 'her eleman için yapılacak bir işlevi yürüt',
+        name: "forEach",
+        shortDesc: "her eleman için yapılacak bir işlevi yürüt",
         desc:
-          'ForEach () yöntemi, her dizi elemanı için bir kez sağlanan bir işlevi yürütür.',
+          "ForEach () yöntemi, her dizi elemanı için bir kez sağlanan bir işlevi yürütür.",
         example: `arr.forEach((element) => {<br>
         <span>&nbsp;&nbsp;</span>console.log(element)<br>
         });`,
@@ -169,20 +167,19 @@ export default {
         8`
       },
       {
-        name: 'map',
-        shortDesc:
-          'bir fonksiyon ile her ögeden yeni bir dizi oluştur',
+        name: "map",
+        shortDesc: "bir fonksiyon ile her ögeden yeni bir dizi oluştur",
         desc:
-          'Bu dizideki her öğede sağlanan bir fonksiyon çağırmanın sonuçlarıyla yeni bir dizi oluşturur.',
+          "Bu dizideki her öğede sağlanan bir fonksiyon çağırmanın sonuçlarıyla yeni bir dizi oluşturur.",
         example: `let map = arr.map(x => x + 1);<br>
         console.log(map);`,
         output: `[6, 2, 9]`
       },
       {
-        name: 'entries',
-        shortDesc: 'bir yineleyici obje oluştur',
+        name: "entries",
+        shortDesc: "bir yineleyici obje oluştur",
         desc:
-          'Dizideki her dizinin anahtar / değer çiftlerini içeren yeni bir Array Iterator nesnesi döndürür. Yineleyiciler için, bununla birlikte kullanılan diğer yöntemlerin yanı sıra <code> values </ code> ve <code> keys </ code> gibi birçok kullanım alanı vardır.',
+          "Dizideki her dizinin anahtar / değer çiftlerini içeren yeni bir Array Iterator nesnesi döndürür. Yineleyiciler için, bununla birlikte kullanılan diğer yöntemlerin yanı sıra <code> values </ code> ve <code> keys </ code> gibi birçok kullanım alanı vardır.",
         example: `let iterator = arr.entries();<br>
         console.log(iterator.next().value);`,
         output: `[0, 5]<br>
@@ -193,61 +190,61 @@ export default {
     find: {
       single: [
         {
-          name: 'includes',
-          shortDesc: 'belli bir element var ise',
+          name: "includes",
+          shortDesc: "belli bir element var ise",
           desc:
-            'Bir dizinin belirli bir öğeyi içerip içermediğini, uygun olduğunda true veya false döndürüp döndürmeyeceğini belirler.',
+            "Bir dizinin belirli bir öğeyi içerip içermediğini, uygun olduğunda true veya false döndürüp döndürmeyeceğini belirler.",
           example: `console.log(arr.includes(1));`,
           output: `true`
         },
         {
-          name: 'indexOf',
-          shortDesc: 'bir elemanın ilk indeksini bulur',
+          name: "indexOf",
+          shortDesc: "bir elemanın ilk indeksini bulur",
           desc:
-            'Belirli bir öğenin dizide bulunabileceği ilk dizini bulur veya mevcut değilse -1 değerini döndürür.',
+            "Belirli bir öğenin dizide bulunabileceği ilk dizini bulur veya mevcut değilse -1 değerini döndürür.",
           example: `console.log(arr.indexOf(5));`,
           output: `0`
         },
         {
-          name: 'lastIndexOf',
-          shortDesc: 'bir elemanın son indeksini bulur',
+          name: "lastIndexOf",
+          shortDesc: "bir elemanın son indeksini bulur",
           desc:
-            'Dizideki bir öğenin belirtilen değere eşit olan son (en büyük) indeksini veya hiçbiri bulunamazsa -1 değerini döndürür.',
+            "Dizideki bir öğenin belirtilen değere eşit olan son (en büyük) indeksini veya hiçbiri bulunamazsa -1 değerini döndürür.",
           example: `console.log(arr.lastIndexOf(5));`,
           output: `0`
         },
         {
-          name: 'find',
-          shortDesc: 'bir koşulu karşılayan ilk durum',
+          name: "find",
+          shortDesc: "bir koşulu karşılayan ilk durum",
           desc:
-            'Dizideki bir öğe sağlanan test işlevini karşılarsa veya bulunmazsa tanımlanmamışsa dizideki bulunan değeri döndürür. <Code> findIndex () </ code> öğesine benzer, ancak indeks yerine öğeyi döndürür.',
+            "Dizideki bir öğe sağlanan test işlevini karşılarsa veya bulunmazsa tanımlanmamışsa dizideki bulunan değeri döndürür. <Code> findIndex () </ code> öğesine benzer, ancak indeks yerine öğeyi döndürür.",
           example: `let isTiny = (el) => el < 2;<br>
           console.log(arr.find(isTiny));`,
           output: `1`
         },
         {
-          name: 'findIndex',
-          shortDesc: 'bir koşulu karşılayan bir elemanın ilk indeksi',
+          name: "findIndex",
+          shortDesc: "bir koşulu karşılayan bir elemanın ilk indeksi",
           desc:
-            'Sağlanan test işlevini karşılayan dizideki ilk öğenin dizinini döndürür. Aksi takdirde -1 iade edilir. <Code> find () </ code> öğesine benzer, ancak öğenin yerine dizini döndürür..',
+            "Sağlanan test işlevini karşılayan dizideki ilk öğenin dizinini döndürür. Aksi takdirde -1 iade edilir. <Code> find () </ code> öğesine benzer, ancak öğenin yerine dizini döndürür..",
           example: `let isBig = (el) => el > 6;<br>
           console.log(arr.findIndex(isBig));`,
           output: `2`
         },
         {
-          name: 'reduce',
-          shortDesc: 'diziden bir değeri indirge (Soldan sağa)',
+          name: "reduce",
+          shortDesc: "diziden bir değeri indirge (Soldan sağa)",
           desc:
-            'Bir akümülatöre ve dizinin her bir değerine (soldan sağa) bir işlevi tek bir değere indirgemek için uygulayın.',
+            "Bir akümülatöre ve dizinin her bir değerine (soldan sağa) bir işlevi tek bir değere indirgemek için uygulayın.",
           example: `let reducer = (a, b) => a + b;<br>
           console.log(arr.reduce(reducer));`,
           output: `14`
         },
         {
-          name: 'reduceRight',
-          shortDesc: 'diziden bir değeri indirge (Sağdan sola)',
+          name: "reduceRight",
+          shortDesc: "diziden bir değeri indirge (Sağdan sola)",
           desc:
-            'Bir akümülatöre ve dizinin her bir değerine (sağdan sola) bir işlevi tek bir değere indirgemek için uygulayın.',
+            "Bir akümülatöre ve dizinin her bir değerine (sağdan sola) bir işlevi tek bir değere indirgemek için uygulayın.",
           example: `[arr, [0, 1]].reduceRight((a, b) => {<br>
           <span>&nbsp;&nbsp;</span>return a.concat(b)<br>
           }, [])`,
@@ -256,28 +253,28 @@ export default {
       ],
       many: [
         {
-          name: 'filter',
-          shortDesc: 'bir koşula göre filtreleme yap',
+          name: "filter",
+          shortDesc: "bir koşula göre filtreleme yap",
           desc:
-            'Sağlanan filtreleme işlevinin doğru olarak döndüğü bu dizinin tüm öğelerini içeren yeni bir dizi oluşturur.',
+            "Sağlanan filtreleme işlevinin doğru olarak döndüğü bu dizinin tüm öğelerini içeren yeni bir dizi oluşturur.",
           example: `let filtered = arr.filter(el => el > 4);<br>
           console.log(filtered)`,
           output: `[5, 8]`
         },
         {
-          name: 'every',
-          shortDesc: 'her eleman için bir koşulu kontrol et',
+          name: "every",
+          shortDesc: "her eleman için bir koşulu kontrol et",
           desc:
-            'Bu dizideki her öğe sağlanan test işlevini karşılarsa true değerini döndürür.',
+            "Bu dizideki her öğe sağlanan test işlevini karşılarsa true değerini döndürür.",
           example: `let isSmall = (el) => el < 10;<br>
           console.log(arr.every(isSmall));`,
           output: `true`
         },
         {
-          name: 'some',
-          shortDesc: 'en az bir eleman için koşulu kontrol et',
+          name: "some",
+          shortDesc: "en az bir eleman için koşulu kontrol et",
           desc:
-            'Bu dizideki en az bir eleman sağlanan sınama işlevini karşılarsa true değerini döndürür.',
+            "Bu dizideki en az bir eleman sağlanan sınama işlevini karşılarsa true değerini döndürür.",
           example: `let biggerThan4 = (el) => el > 4;<br>
           console.log(arr.some(biggerThan4));`,
           output: `true`
@@ -285,4 +282,4 @@ export default {
       ]
     }
   }
-}
+};
